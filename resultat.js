e9cdbb6a35bb2a85e62ef372c5e4610bdedcdb02
ConnyKlasson <conny.klasson@gmail.com>
2020-10-10 21:48:42 +0200

    Update resultat.js

--- resultat.js
+++ resultat.js
@@ -21,7 +21,7 @@ function resultatRuta(x, y, rad){
     rect(this.x, this.y, this.bredd, this.höjd);
 
     if (rad < 7){
-      console.log('R', rad , 'BR', this.bonusRäkning, 'BI', blurbIndex, 'BS', blurbSumma);
+      console.log('R:', rad , 'BR', this.bonusRäkning, 'BI', blurbIndex, 'BS', blurbSumma);
       noStroke();
       fill(255);
       if (rad == 6){
