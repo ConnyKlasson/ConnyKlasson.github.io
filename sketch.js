10081cb89edb2fefcd2d5ad760c45baa0a5723a8
ConnyKlasson <conny.klasson@gmail.com>
2020-10-11 18:38:55 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -108,12 +108,13 @@ function setup() {
 }
 
 function ritaStatistik(){
+  console.log('riteStatistik');
   fill(255);
   rect(25, 709, 355, 30);
   fill(0);
   textAlign(CENTER);
   for (let i = 0; i < statistik.length; i++){
-    text(statistik[i], 60 + (54 * i), 728);
+    text(statistik[i], 60 + (55 * i), 728);
   }
 }
 
