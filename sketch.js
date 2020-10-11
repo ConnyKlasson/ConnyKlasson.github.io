263ca278c05594df328a9435e0856f8581035f73
ConnyKlasson <conny.klasson@gmail.com>
2020-10-11 16:06:35 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -107,7 +107,7 @@ function setup() {
 function ritaStatistik(){
   console.log("Rita Statistik");
   fill(255);
-  rect(25, 655, 335, 60);
+  rect(25, 660, 340, 60);
   fill(0);
   textAlign(LEFT);
   text("Ettor:", 30, 710);
