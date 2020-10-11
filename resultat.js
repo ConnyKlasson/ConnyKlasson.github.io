e464f2e5578470cbdc8eea7c704a1e910d29c1b5
ConnyKlasson <conny.klasson@gmail.com>
2020-10-11 23:38:14 +0200

    Update resultat.js

--- resultat.js
+++ resultat.js
@@ -82,7 +82,7 @@ fyllPrel = function(){
       if (k < 6){
         resultatRad[k].blurb =  ((k + 1) * 3) * -1;;
       }
-      resultatRad[k].rita();
+      //resultatRad[k].rita();
     }
   }
 
@@ -185,7 +185,7 @@ fyllPrel = function(){
       if (prelResultat[j] > 0 && !resultatRad[j].klar){
         resultatRad[j].resultat = prelResultat[j];
         resultatRad[j].blurb = resultatRad[j].blurb + prelResultat[j];
-        //resultatRad[j].rita();
+        resultatRad[j].rita();
       }
   }
 }
